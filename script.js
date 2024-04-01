a568655c9c187860d51fa421ba1b72c5550882eb
johntatsu <128668934+johntatsu@users.noreply.github.com>
2024-04-01 15:16:01 +0800

    Add files via upload
    Added all files of the activity.

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.querySelector(".dropbtn").addEventListener("click", function () {
+//   document.querySelector(".dropdown-content").style.display =
+//     document.querySelector(".dropdown-content").style.display === "block"
+//       ? "none"
+//       : "block";
+// });
